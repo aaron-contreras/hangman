0e8cffabb39fe0e7bd5e3f1ea02b8010e024d773
Aaron Contreras <aaronlcaq@gmail.com>
2020-06-21 17:14:12 -0500

    Refactor game loop on rubocop standards

--- lib/game.rb
+++ lib/game.rb
@@ -12,17 +12,9 @@ class Game
   attr_accessor :guess, :remaining_letters
 
   GAME_MODES = %w[n l].freeze
-  SAVED_GAMES = Dir.glob './saved_games/*.json'
-
-  def game_list
-    SAVED_GAMES.each_with_index.reduce({}) do |hash, (file, index)|
-      hash[index + 1] = file
-      hash
-    end
-  end
+  IN_GAME_OPTIONS = %w[/exit /save].freeze
 
   def initialize
-    clear_screen
     puts start_screen
     @secret_word = select_secret_word
     @remaining_letters = @secret_word.clone
@@ -32,21 +24,15 @@ class Game
   end
 
   def start
+    check_for_saved_games_directory
     mode = gets.chomp.downcase until GAME_MODES.include? mode
+    clear_screen
+
     if mode == 'n'
       play
     else
-      clear_screen
-      puts load_a_game_screen SAVED_GAMES
-      file_number = gets.chomp until game_list.key?(file_number.to_i) || file_number == '/back' || file_number == '/exit'
-
-      if file_number == '/back'
-        Game.new
-      elsif game_list.key? file_number.to_i
-        saved_game = File.read game_list[file_number.to_i]
-        deserialize saved_game
-        play
-      end
+      puts load_a_game_screen saved_games
+      proceed_with valid_load_screen_option
     end
   end
 
@@ -69,9 +55,20 @@ class Game
     incorrect_guesses.length == 7
   end
 
+  def game_is_over?
+    no_more_guesses? || word_cracked?
+  end
+
   def no_matches?
     previous_remaining_letters = remaining_letters
-    self.remaining_letters = remaining_letters.gsub guess, ''
+    self.remaining_letters = if word_cracked?
+                               ' '
+                             elsif guess.length == 1
+                               remaining_letters.gsub(guess, '')
+                             else
+                               remaining_letters
+                             end
+
     remaining_letters == previous_remaining_letters
   end
 
@@ -83,35 +80,55 @@ class Game
     clear_screen
     puts hanging_man_drawing incorrect_guesses.length
     puts word_status
-    print next_move_prompt
+    print guess_prompt unless game_is_over?
+  end
+
+  def proceed_with(selected_option)
+    return if selected_option == '/exit'
+
+    if selected_option == '/back'
+      Game.new
+    elsif game? selected_option
+      game_key = selected_option.to_i
+      deserialize File.read(game_list[game_key])
+      play
+    end
+  end
+
+  def update_incorrect_guesses(guess)
+    incorrect_guesses << guess if no_matches?
+  end
+
+  def save
+    print save_game_screen
+    save_game_to_file
+    puts succesful_save
+    sleep(1)
+  end
+
+  def run_game_loop
+    until game_is_over?
+      update_display
+      self.guess = gets.chomp.strip.downcase
+
+      if IN_GAME_OPTIONS.include? guess
+        break if guess == '/exit'
+
+        save
+        next
+      end
+
+      update_incorrect_guesses guess
+    end
   end
 
   def play
+    run_game_loop
+
     update_display
-    until no_more_guesses? || word_cracked?
-      self.guess = gets.chomp.downcase
-      return if guess == '/exit'
+    game_over_message word_cracked?, secret_word
 
-      if guess == '/save'
-        clear_screen
-        puts save_game_screen
-        filename = loop do
-          filename = ''
-          filename = gets.chomp while filename.empty?
-          filename += ".#{EXTENSION}"
-          break filename unless File.exist? "./saved_games/#{filename}"
-
-          puts "You've already saved game with this name"
-        end
-
-        File.open("./saved_games/#{filename}", 'w') { |file| file.puts serialize }
-      end
-
-      incorrect_guesses << guess if no_matches?
-      update_display
-    end
-
-    puts game_over_message word_cracked?
-    puts "The word was #{secret_word}"
+    end_of_game_choice = gets.chomp.to_i until [1, 2].include? end_of_game_choice
+    Game.new if end_of_game_choice == 1
   end
 end
