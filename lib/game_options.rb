fb78ee41d09007688bad481083d6bcc29fc543a2
Aaron Contreras <aaronlcaq@gmail.com>
2020-06-21 20:11:30 -0500

    Add game_options module

---
+++ lib/game_options.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Includes the options prompted for throughout gameplay
+module GameOptions
+  START_SCREEN_OPTIONS = %w[n s].freeze
+  IN_GAME_OPTIONS = %w[/exit /save].freeze
+  END_OF_GAME_OPTIONS = [1, 2].freeze
+end
